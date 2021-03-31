b6d6294ff09a65a0ba7a4ddac5b5ddb10c4a78f6
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-03-31 11:22:13 -0400

    useState hooks

---
+++ src/Home.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import BlogList from './BlogList';
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {title: 'My new website', body: 'content 1', author: 'mario', id:1 },
+    {title: 'Welcome party', body: 'content 2', author: 'yoshi', id:2 },
+    {title: 'Web dev top tips', body: 'content 3', author: 'mario', id:3 }
+  ])
+  
+  const [name, setName] = useState('mario')
+  // let name = 'mario'
+
+  const [age, setAge] = useState(25)
+
+  const handleClick = () => {
+    setName('luigi')
+    setAge(30)
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log ('hello ' + name, e.target)
+  }
+  return ( 
+    <div className="home">
+      <h2>Home Page</h2>
+      <p>{ name } is {age} years old</p>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e)=> handleClickAgain('mario', e)}>Click me again</button>
+      <BlogList />
+    </div>
+   );
+}
+ 
+export default Home;
